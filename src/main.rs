53892f570ae0bd9f986da4c4d0fe4bc40755afd0
Saiki <GSaiki26@gmail.com>
2023-12-28 20:32:40 -0300

    Added: game queue

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,119 @@
 // Libs
 use reqwest::{Client, Error};
 
-use models::{catalog::CatalogResponse, webhook_message::WebhookMessage};
+use models::{
+    cache::Cache,
+    catalog::{CatalogResponse, StoreGame},
+    webhook_message::WebhookMessage,
+};
 
 mod models;
 
 // Functions
-async fn notify(catalog: &CatalogResponse) -> Result<(), Error> {
+/**
+ * A method to check new games.
+*/
+async fn check_new_games(cache: &mut Cache, games: &Vec<StoreGame>) {
+    println!("\nChecking new games...");
+
+    for game in games {
+        println!("Checking \"{}\" from catalog...", game.title);
+
+        // Check if the game is in the cache.
+        if cache.get_game(&game.id).is_some() {
+            println!("\"{}\" is already in the cache.", game.title);
+            continue;
+        }
+
+        // Notify the webhook.
+        match notify_game(game).await {
+            Ok(_) => cache.add_game(game),
+            Err(msg) => println!("Couldn\'t notify the webhook: {}", msg),
+        }
+    }
+}
+
+/**
+ * A method to check the games that are stored in the cache.
+*/
+fn remove_old_games(cache: &mut Cache, games: Vec<StoreGame>) {
+    println!("\nChecking old games...");
+
+    let cached_games = cache.get_all_games();
+    for cached_game in cached_games {
+        println!("Checking \"{}\" from cache...", cached_game.title);
+
+        // Check if the game is in the catalog.
+        if games.iter().any(|game| game.id == cached_game.id) {
+            println!("\"{}\" is still in the catalog.", cached_game.title);
+            continue;
+        }
+
+        // If the game if not in the catalog, remove it from the cache.
+        println!(
+            "\"{}\" is not in the catalog anymore. Removing...",
+            cached_game.title
+        );
+        cache.remove_game(&cached_game.id);
+    }
+}
+
+/**
+ * A method to notify the webhook about a new game.
+*/
+async fn notify_game(game: &StoreGame) -> Result<(), Error> {
     let webhook_url = std::env::var("WEBHOOK_URL").expect("Couldn\'t load the WEBHOOK_URL.");
 
-    for game in catalog.data.catalog.search_store.elements.clone() {
-        println!("Sending to the webhook informations about: {}", game.title);
+    println!("Sending to the webhook informations about: {}", game.title);
 
-        // Create the webhook message.
-        let req_body = WebhookMessage::new(&game);
+    // Create the webhook message.
+    let req_body = WebhookMessage::new(game);
 
-        // Send the webhook message. req_body is a json.
-        let res = Client::new()
-            .post(webhook_url.clone())
-            .json(&req_body)
-            .send()
-            .await;
+    // Send the webhook message. req_body is a json.
+    let res = Client::new()
+        .post(webhook_url.clone())
+        .json(&req_body)
+        .send()
+        .await;
 
-        match res {
-            Ok(response) => {
-                println!("{}", response.text().await?);
-                println!("Webhook message sent.");
-            }
-            Err(msg) => return Err(msg),
+    match res {
+        Ok(response) => {
+            println!("{}", response.text().await?);
+            println!("Webhook message sent.");
         }
+        Err(msg) => return Err(msg),
     }
 
     Ok(())
 }
 
-// Main
-#[tokio::main]
-async fn main() {
-    // Get the current catalog.
+/**
+ * A method to get the current catalog from EpicGames.
+*/
+async fn get_catalog() -> CatalogResponse {
     println!("Getting the current catalog...");
 
+    // Get the current catalog.
     let url = std::env::var("CATALOG_URL").expect("CATALOG_URL not found.");
-    let catalog = Client::new()
+    Client::new()
         .get(url)
         .send()
         .await
         .expect("Couldn\'t connect to epicGames.")
         .json::<CatalogResponse>()
         .await
-        .expect("Couldn\'t parse the response.");
+        .expect("The format of the catalog is not valid. Please check the CATALOG_URL.")
+}
 
+// Main
+#[tokio::main]
+async fn main() {
+    let catalog = get_catalog().await;
     let mut cache = models::cache::Cache::new();
-    let cache_catalog: Option<CatalogResponse> = cache.get_catalog();
+    let games = catalog.data.catalog.search_store.elements.clone();
 
-    // Check if there's a new catalog.
-    if cache_catalog.is_none() || catalog.data != cache_catalog.unwrap().data {
-        println!("The cached catalog has updates.");
-        let _ = match notify(&catalog).await {
-            Ok(_) => cache.set_catalog(&catalog),
-            Err(msg) => panic!("{}", msg),
-        };
-    } else {
-        println!("The current catalog has any changes.");
-    }
+    check_new_games(&mut cache, &games).await;
+    remove_old_games(&mut cache, games);
 }
 
 // fn main() {}
--- src/models/cache.rs
+++ src/models/cache.rs
@@ -1,57 +1,109 @@
 // Libs
-use std::io::{Seek, Write};
+use std::fs::File;
+use std::io::Write;
 
-use super::catalog::CatalogResponse;
+use super::catalog::StoreGame;
 
 // Structs
 #[derive(Debug)]
 pub struct Cache {
-    file: std::fs::File,
+    filepath: String,
 }
 
 // Implementations
 impl Cache {
     pub fn new() -> Self {
+        // get the filepath and check if the file exists.
         let filepath = std::env::var("CACHE_PATH").expect("CACHE_PATH not found.");
-        let filepath = std::path::Path::new(&filepath);
-        let mut file = std::fs::OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(filepath)
-            .expect("Couldn\'t open the cache file.");
 
-        // If the file is empty, write an empty json.
-        if file.metadata().unwrap().len() == 0 {
-            file.write_all(b"{}")
+        // Create the file if it doesn't exist.
+        if !std::path::Path::new(&filepath).exists() {
+            println!("Creating the cache file...");
+            let mut cache_file = File::create(&filepath).expect("Couldn\'t create the cache file.");
+            cache_file
+                .write_all(b"[]")
                 .expect("Couldn\'t write to the cache file.");
-            file.flush().expect("Couldn\'t flush the cache file.");
+            cache_file.flush().expect("Couldn\'t flush the cache file.");
         }
 
-        Self { file }
+        Self { filepath }
     }
 
-    pub fn get_catalog(&self) -> Option<CatalogResponse> {
-        println!("Getting the cached catalog...");
+    /**
+     * A method to get a game from the cache.
+     */
+    pub fn get_game(&self, game_id: &str) -> Option<StoreGame> {
+        println!("Getting from the cache the game#{}...", game_id);
 
         // Read the entire cache.
-        serde_json::from_reader(&self.file).ok()
+        let cache_file = File::open(&self.filepath).expect("Couldn\'t read the cache.");
+        let cached_games: Vec<StoreGame> =
+            serde_json::from_reader(cache_file).expect("Couldn\'t parse the cache.");
+
+        // Return the game if it exists.
+        cached_games.into_iter().find(|game| game.id == game_id)
     }
 
-    pub fn set_catalog(&mut self, catalog: &CatalogResponse) -> Result<(), String> {
-        println!("Setting the cached catalog...");
+    /**
+     * A method to get a game from the cache.
+     */
+    pub fn get_all_games(&self) -> Vec<StoreGame> {
+        println!("Getting all games from the cache...");
 
-        // Write the entire file with the new catalog.
-        self.file
-            .seek(std::io::SeekFrom::Start(0))
-            .expect("Couldn\'t seek the cache file.");
+        // Read the entire cache.
+        let cache_file = File::open(&self.filepath).expect("Couldn\'t read the cache.");
+        serde_json::from_reader(cache_file).expect("Couldn\'t parse the cache.")
+    }
 
-        let catalog = serde_json::to_string(&catalog).unwrap();
-        self.file
-            .write_all(catalog.as_bytes())
+    /**
+     * A method to add a game to the cache.
+     */
+    pub fn add_game(&self, game: &StoreGame) {
+        println!("Adding to the cache the game#{}...", game.id);
+
+        // Read the entire cache.
+        let cache_file = File::open(&self.filepath).expect("Couldn\'t read the cache.");
+        let mut cached_games: Vec<StoreGame> =
+            serde_json::from_reader(cache_file).expect("Couldn\'t parse the cache.");
+
+        cached_games.push(game.clone());
+
+        // Write the cache back to the file.
+        let mut cache_file = File::create(&self.filepath).expect("Couldn\'t read the cache.");
+        cache_file
+            .set_len(0)
+            .expect("Couldn\'t clear the cache file.");
+        serde_json::to_writer(&cache_file, &cached_games)
             .expect("Couldn\'t write to the cache file.");
-        self.file.flush().expect("Couldn\'t flush the cache file.");
 
-        Ok(())
+        // Flush the file.
+        cache_file.flush().expect("Couldn\'t flush the cache file.");
+    }
+
+    /**
+     * A method to remove a game from the cache.
+     */
+    pub fn remove_game(&self, game_id: &str) {
+        println!("Removing from the cache the game#{}...", game_id);
+
+        // Read the entire cache.
+        let cache_file = File::open(&self.filepath).expect("Couldn\'t read the cache.");
+        let mut cached_games: Vec<StoreGame> =
+            serde_json::from_reader(cache_file).expect("Couldn\'t parse the cache.");
+
+        // Remove the game from the cache.
+        cached_games.retain(|game| game.id != game_id);
+
+        // Write the cache back to the file.
+        let mut cache_file = File::create(&self.filepath).expect("Couldn\'t write the cache.");
+        cache_file
+            .set_len(0)
+            .expect("Couldn\'t clear the cache file.");
+
+        // Write the cache back to the file.
+        match serde_json::to_writer(&cache_file, &cached_games) {
+            Ok(_) => cache_file.flush().expect("Couldn\'t flush the cache file."),
+            Err(_) => println!("Couldn\'t remove the game from the cache."),
+        }
     }
 }
