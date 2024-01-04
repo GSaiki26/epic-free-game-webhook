d907de7dd06de5d9c1a0bfa74ee7084f769ee075
Saiki <44827854+GSaiki26@users.noreply.github.com>
2024-01-04 15:26:18 -0300

    Fixed: Bad cmp and improved embed message (#10)

--- src/models/catalog.rs
+++ src/models/catalog.rs
@@ -1,37 +1,38 @@
 // Libs
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
 // Structs
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct CatalogResponse {
     pub data: Data,
 }
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct Data {
     #[serde(rename = "Catalog")]
     pub catalog: CatalogData,
 }
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct CatalogData {
     #[serde(rename = "searchStore")]
     pub search_store: SearchStore,
 }
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct SearchStore {
     pub elements: Vec<StoreGame>,
 }
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[derive(Clone, Deserialize, PartialEq, Serialize)]
 pub struct StoreGame {
     pub id: String,
     pub title: String,
     pub description: String,
 
     #[serde(rename = "productSlug")]
-    pub product_slug: String,
+    pub product_slug: Option<String>,
 
     #[serde(rename = "effectiveDate")]
     pub effective_date: String,
@@ -45,8 +46,27 @@ pub struct StoreGame {
     pub url: Option<String>,
 }
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[derive(Clone, Deserialize, PartialEq, Serialize)]
 pub struct KeyImage {
     pub r#type: String,
     pub url: String,
+}
+
+// Functions
+/**
+ * A method to get the current catalog from EpicGames.
+*/
+pub async fn get_catalog() -> CatalogResponse {
+    println!("Getting the current catalog...");
+
+    // Get the current catalog.
+    let url = std::env::var("CATALOG_URL").expect("CATALOG_URL not found.");
+    Client::new()
+        .get(url)
+        .send()
+        .await
+        .expect("Couldn\'t connect to epicGames.")
+        .json::<CatalogResponse>()
+        .await
+        .expect("Couldn\'t parse the CATALOG_URL body.")
 }
